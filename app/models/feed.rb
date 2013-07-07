67a6b7f6fa7195bc51001d59fa63666af34f51f3
Suo <darthsuo@gmail.com>
2013-07-07 00:02:19 -0400

    remove demon counter_cache

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,7 +1,7 @@
 class Feed < ActiveRecord::Base
   attr_accessible :description, :link, :title, :user_id
   belongs_to :user, inverse_of: :feeds
-  has_many :items, inverse_of: :feed, counter_cache: true
+  has_many :items, inverse_of: :feed
 
   validates :title, presence: true, length: { maximum: 40 }
   validates :link, presence: true
